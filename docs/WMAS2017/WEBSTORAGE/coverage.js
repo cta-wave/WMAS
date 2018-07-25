1f5c91ee3712de615de6ae2716163a1b7eefcd69
Martin Lasak <martin.lasak@fokus.fraunhofer.de>
2018-07-25 09:19:36 +0200

    Add missing gap report files

---
+++ docs/WMAS2017/WEBSTORAGE/coverage.js
@@ -0,0 +1,8 @@
+var coverage = {
+  "the-storageevent-interface": 100,
+  "the-storage-interface": 100,
+  "the-localstorage-attribute": 100,
+  "the-sessionstorage-attribute": 100,
+  "threads": 0,
+  "disk-space": 0,
+}
