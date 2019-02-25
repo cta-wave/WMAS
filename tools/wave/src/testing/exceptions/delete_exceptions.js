c8b40cead8a29796fc6c0f2971c2ec2879553ebf
Mehdi Ben Fadhel <mehdi.ben.fadhel@fokus.fraunhofer.de>
2019-02-25 18:03:26 +0100

    Remove logs

--- tools/wave/src/testing/exceptions/delete_exceptions.js
+++ tools/wave/src/testing/exceptions/delete_exceptions.js
@@ -9,7 +9,6 @@ removeTestFiles();
 async function removeTestFiles() {
 	for (var i = 0; i < exceptions.length; i++){
 		var path = exceptions[i];
-		console.log(path);
 		await deleteTest(path);
 	}
 }
