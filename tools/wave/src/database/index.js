b71f379c09b8980556661ed72d95a8d921cf282a
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2019-02-26 15:07:37 +0100

    fixed wave database bugs

--- tools/wave/src/database/index.js
+++ tools/wave/src/database/index.js
@@ -10,11 +10,11 @@ const DEFAULT_DATABASE_DIRECTORY_PATH = ".";
 class Database {
   constructor({ dbCompactionInterval }) {
     this._db = {};
-    this.dbCompactionInterval = dbCompactionInterval;
-    this._sessionsDatabase = new SessionsDatabase({
+    this._dbCompactionInterval = dbCompactionInterval;
+    this._bindSessionsDatabase({
       compactionInterval: dbCompactionInterval
     });
-    this._resultsDatabase = new ResultsDatabase({
+    this._bindResultsDatabase({
       compactionInterval: dbCompactionInterval
     });
     this._testsDatabase = new TestsDatabase();
@@ -30,55 +30,29 @@ class Database {
     const testsDirectoryPath = path.join(databaseDirectoryPath, "tests");
     await this._testsDatabase.initialize(testsDirectoryPath);
     await this._sessionsDatabase.initialize({
-      databaseDirectoryPath,
+      directoryPath: databaseDirectoryPath,
       resultsDatabase: this._resultsDatabase,
       testsDatabase: this._testsDatabase
     });
   }
 
-  async createSession() {
-    const sessionDatabase = this._sessionsDatabase;
-    return sessionDatabase.createSession.apply(sessionDatabase, arguments);
+  _bindSessionsDatabase(options) {
+    const sessionsDatabase = new SessionsDatabase(options);
+    this.createSession = sessionsDatabase.createSession.bind(sessionsDatabase);
+    this.readSession = sessionsDatabase.readSession.bind(sessionsDatabase);
+    this.readSessions = sessionsDatabase.readSessions.bind(sessionsDatabase);
+    this.updateSession = sessionsDatabase.updateSession.bind(sessionsDatabase);
+    this.deleteSession = sessionsDatabase.deleteSession.bind(sessionsDatabase);
+    this.findTokens = sessionsDatabase.findTokens.bind(sessionsDatabase);
+    this._sessionsDatabase = sessionsDatabase;
   }
 
-  async readSession() {
-    const sessionDatabase = this._sessionsDatabase;
-    return sessionDatabase.readSession.apply(sessionDatabase, arguments);
-  }
-
-  async readSessions() {
-    const sessionDatabase = this._sessionsDatabase;
-    return sessionDatabase.readSessions.apply(sessionDatabase, arguments);
-  }
-
-  async updateSession() {
-    const sessionDatabase = this._sessionsDatabase;
-    return sessionDatabase.updateSession.apply(sessionDatabase, arguments);
-  }
-
-  async deleteSession() {
-    const sessionDatabase = this._sessionsDatabase;
-    return sessionDatabase.deleteSession.apply(sessionDatabase, arguments);
-  }
-
-  async findTokens() {
-    const sessionDatabase = this._sessionsDatabase;
-    return sessionDatabase.findTokens.apply(sessionDatabase, arguments);
-  }
-
-  async createResult() {
-    const resultsDatabase = this._resultsDatabase;
-    return resultsDatabase.createResult.apply(resultsDatabase, arguments);
-  }
-
-  async readResults() {
-    const resultsDatabase = this._resultsDatabase;
-    return resultsDatabase.readResults.apply(resultsDatabase, arguments);
-  }
-
-  async _deleteResults() {
-    const resultsDatabase = this._resultsDatabase;
-    return resultsDatabase.deleteResults.apply(resultsDatabase, arguments);
+  _bindResultsDatabase(options) {
+    const resultsDatabase = new ResultsDatabase(options);
+    this.createResult = resultsDatabase.createResult.bind(resultsDatabase);
+    this.readResults = resultsDatabase.readResults.bind(resultsDatabase);
+    this.deleteResults = resultsDatabase.deleteResults.bind(resultsDatabase);
+    this._resultsDatabase = resultsDatabase;
   }
 }
 
