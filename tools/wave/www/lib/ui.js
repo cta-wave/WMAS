d346032074d84a1f43491b8f840a178082a4361c
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2019-03-05 16:20:49 +0100

    refreshing session details in session result view on completion; added capability to confirm adding a session by pressing enter in the results overview;

--- tools/wave/www/lib/ui.js
+++ tools/wave/www/lib/ui.js
@@ -4,36 +4,45 @@ const UI = {
     const elementType = config.element || "div";
     const element = document.createElement(elementType);
 
-    if (config.hasOwnProperty("className"))
-      element.setAttribute("class", config.className);
-    if (config.hasOwnProperty("id")) element.setAttribute("id", config.id);
-    if (config.hasOwnProperty("text")) element.innerText = config.text;
-    if (config.hasOwnProperty("html")) element.innerHTML = config.html;
-    if (config.hasOwnProperty("style"))
-      element.setAttribute("style", config.style);
-    if (config.hasOwnProperty("onclick"))
-      element.onclick = config.onclick.bind(element);
-    if (config.hasOwnProperty("onchange"))
-      element.onchange = config.onchange.bind(element);
-    if (config.hasOwnProperty("src")) element.setAttribute("src", config.src);
-    if (elementType === "input") {
-      if (config.hasOwnProperty("type"))
-        element.setAttribute("type", config.type);
-    }
-    if (config.children) {
-      config.children.forEach(child =>
-        element.appendChild(UI.createElement(child))
-      );
-    }
-    if (config.hasOwnProperty("disabled") && config.disabled) {
-      element.setAttribute("disabled", true);
-    }
-    if (config.hasOwnProperty("placeholder")) {
-      element.setAttribute("placeholder", config.placeholder);
-    }
-    if (config.hasOwnProperty("title")) {
-      element.setAttribute("title", config.title);
-    }
+    Object.keys(config).forEach(property => {
+      const value = config[property];
+      switch (property.toLowerCase()) {
+        case "id":
+        case "src":
+        case "style":
+        case "placeholder":
+        case "title":
+          element.setAttribute(property, value);
+          return;
+        case "classname":
+          element.setAttribute("class", value);
+          return;
+        case "text":
+          element.innerText = value;
+          return;
+        case "html":
+          element.innerHTML = value;
+          return;
+        case "onclick":
+          element.onclick = value.bind(element);
+          return;
+        case "onchange":
+          element.onchange = value.bind(element);
+          return;
+        case "onkeydown":
+          element.onkeydown = value.bind(element);
+          return;
+        case "type":
+          if (elementType === "input") element.setAttribute("type", value);
+          return;
+        case "children":
+          value.forEach(child => element.appendChild(UI.createElement(child)));
+          return;
+        case "disabled":
+          if (value) element.setAttribute("disabled", true);
+          return;
+      }
+    });
     return element;
   },
   getElement: id => {
