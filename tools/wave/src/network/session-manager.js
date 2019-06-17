ac5d87705185f6b62542eeb4fe2eeba0389bc405
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2019-06-17 16:52:33 +0200

    implemented statistics script; changed wave script to forward all parameters to wave-cli.js

--- tools/wave/src/network/session-manager.js
+++ tools/wave/src/network/session-manager.js
@@ -65,7 +65,8 @@ class SessionManager {
       testTimeout,
       tests,
       status: Session.RUNNING,
-      referenceTokens
+      referenceTokens,
+      dateStarted: Date.now()
     });
     await this._database.createSession(session);
     this._sessions.push(session);
