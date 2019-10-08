f181b50f9e34210f990394e8d7f572d00fb4c091
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2019-10-08 18:34:35 +0200

    fixed wrong timeout for manual tests

--- tools/wave/src/testing/session-manager.js
+++ tools/wave/src/testing/session-manager.js
@@ -15,8 +15,7 @@ const DEFAULT_TEST_TYPES = [
   TestLoader.MANUAL_TESTS
 ];
 const DEFAULT_AUTOMATIC_TIMEOUT = 60000;
-// const DEFAULT_MANUAL_TIMEOUT = 5 * 60000;
-const DEFAULT_MANUAL_TIMEOUT = 500;
+const DEFAULT_MANUAL_TIMEOUT = 5 * 60000;
 
 /**
  * @module SessionManager
