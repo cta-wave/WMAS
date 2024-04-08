dabeb33e23bfcd28b184df11c71e7b76a73bbc89
Fritz Heiden <fritz.heiden@fokus.fraunhofer.de>
2024-04-08 12:56:06 +0200

    fix: remove session field "type"

--- tools/wave/utils/serializer.py
+++ tools/wave/utils/serializer.py
@@ -21,7 +21,6 @@ def serialize_session(session):
         "is_public": session.is_public,
         "reference_tokens": session.reference_tokens,
         "expiration_date": millis_to_iso(session.expiration_date),
-        "type": session.session_type,
         "malfunctioning_tests": session.malfunctioning_tests
     }
 
